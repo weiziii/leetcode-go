4288fbc3b86ef8e3ed7be0edfa2139f1e5e22dff
wangwei <wangwei@medatc.com>
2018-12-05 17:52:33 +0800

    impl

--- problems/0001.two-sum/two-sum.go
+++ problems/0001.two-sum/two-sum.go
@@ -1,8 +1,17 @@
-package two_sum
+package _001_two_sum
 
 /*
-    by wangwei
-    2018/12/5 16:43
- */
+   by wangwei
+   2018/12/5 16:43
+*/
 
- 
+func twoSum(nums []int, target int) []int {
+	index := make(map[int]int, len(nums))
+	for k, v := range nums {
+		if j, ok := index[target-v]; ok {
+			return []int{j, k}
+		}
+		index[v] = k
+	}
+	return nil
+}
